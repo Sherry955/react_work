a19e8d4a515b177bcf3e74429e522191ebf3eed7
Sherry Shih <jokins82@gmail.com>
2019-08-25 18:48:13 +0800

    Updates

---
+++ precache-manifest.10a1190ac2d7020cb12517d4c59b29a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22491e8f97e5d4a3fa9066ab0e699890",
+    "url": "/react_work/index.html"
+  },
+  {
+    "revision": "8b7b990e57f408232206",
+    "url": "/react_work/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7a2dfeec8fe0cf6b0bb0",
+    "url": "/react_work/static/js/2.c7ad38b3.chunk.js"
+  },
+  {
+    "revision": "8b7b990e57f408232206",
+    "url": "/react_work/static/js/main.b271a674.chunk.js"
+  },
+  {
+    "revision": "60605e918e55630e0f53",
+    "url": "/react_work/static/js/runtime~main.a3cc99d2.js"
+  }
+]);
